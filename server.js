fc3c6399aa699dbff74d697a149291eeeb21219d
Juan Gabriel Fagundez <juangabrielfagundez@192.168.1.4>
2022-09-12 19:16:32 -0300

    routes carts and controllers

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT;
 const productRoutes = require('./api/routes/productsRoutes')
+const cartRoutes = require('./api/routes/cartsRoutes')
 
 app.use(express.json())
 app.use('/api/v1' , productRoutes)
+app.use('/api/v1/carts', cartRoutes)
 
 
 app.listen(PORT, ()=> {
