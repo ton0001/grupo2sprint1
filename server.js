fd11129b0bcdae43a0762a2c155f6993a39f45b1
ton0001 <diegogaston@protonmail.ch>
2022-09-12 11:22:14 -0300

    agregados archivos genericos

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT;
+
+app.use(express.json())
+
+
+app.listen(PORT, ()=> {
+    console.log(`server corriendo en ${PORT}`)
+})
+
