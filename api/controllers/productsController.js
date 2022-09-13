94e00c1567b56edb9affd83fe7c6e09f470d29e5
Juan Gabriel Fagundez <juangabrielfagundez@192.168.1.4>
2022-09-13 16:07:11 -0300

    agregando metodo a product controller

--- api/controllers/productsController.js
+++ api/controllers/productsController.js
@@ -1,54 +1,72 @@
-const express = require('express');
-const fs = require('fs')
-const path = require('path');
-
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const productController = {
-allProduct: function(req,res){
-
+  allProduct: function (req, res) {
     try {
-        if(res.status(200)){
-            const readAllProduct = fs.readFileSync('api/data/products.json','utf-8')
-            const readAllProductParsered = JSON.parse(readAllProduct)  
-            res.send(readAllProductParsered)
-        }else{
-            res.status(500).send("Sever Error")
-        }
-       
-
-    } catch(error) {
-        res.send(error)
-        console.log("Catch error" + error);
+      if (res.status(200)) {
+        const readAllProduct = fs.readFileSync(
+          "api/data/products.json",
+          "utf-8"
+        );
+        const readAllProductParsered = JSON.parse(readAllProduct);
+        res.send(readAllProductParsered);
+      } else {
+        res.status(500).send("Sever Error");
+      }
+    } catch (error) {
+      res.send(error);
+      console.log("Catch error" + error);
     }
-},
+  },
 
-oneProduct: function(req,res){
+  oneProduct: function (req, res) {
     const porductByParams = req.params.id;
 
     try {
-        if(res.status(200).res.send({"Msg": "Ok"})){
-            
-            const readAllProduct = fs.readFileSync('api/data/products.json','utf-8')
-            const readAllProductParsered = JSON.parse(readAllProduct)  
-            res.send(readAllProductParsered)
-
-        }else{
-
-            res.status(500).res.json({"Msg": "Sever Error"})
-        }
-       
-
-    } catch(error) {
-        res.send(error)
-        console.log("Catch error" + error);
+      if (res.status(200).res.send({ Msg: "Ok" })) {
+        const readAllProduct = fs.readFileSync(
+          "api/data/products.json",
+          "utf-8"
+        );
+        const readAllProductParsered = JSON.parse(readAllProduct);
+        res.send(readAllProductParsered);
+      } else {
+        res.status(500).res.json({ Msg: "Sever Error" });
+      }
+    } catch (error) {
+      res.send(error);
+      console.log("Catch error" + error);
     }
-}
+  },
+  listCategory: (req, res) => {
+    let category = req.query.category;
 
+    try{
+      let dbProduct = fs.readFileSync(
+        "api/data/products.json",
+        "utf-8"
+      );
+      dbProduct = JSON.parse(dbProduct);
 
+      let categoryFind = dbProduct.find(el => el.category.toLowerCase() === category.toLowerCase())
+      
+      if(categoryFind){
+        let dbProductFilter = dbProduct.filter(el => el.category.toLowerCase() === category.toLowerCase());
+        res.status(200).send(dbProductFilter);
+      } else {
+        res.status(404).json({
+          msg: 'Not Found'
+        })
+      }
+    } catch(err){
+      console.log(err)
+      res.status(500).json({
+        msg: 'Error interno'
+     });
+    }
+  }
+};
 
-
-
-}
-
-
-module.exports = productController
+module.exports = productController;
--- api/routes/productsRoutes.js
+++ api/routes/productsRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productsController');
 
+router.get('/products', productController.listCategory)
 router.get('/products', productController.allProduct)
 router.get('/products/:id', productController.oneProduct)
-
 
 module.exports = router;
